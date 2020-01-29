bbc34c6270e776cb58533a4b6d56248a075e20e4
sasha walker <sashawalker31@gmail.com>
2020-01-29 14:24:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-  array=["kiwi"]
-  result = array.join
-    return result
-end
-
-  def oxford_comma(array=["kiwi", "durian"])
-    new_result=array.join(" and ")
-    return new_result
+  if array.length == 2
+    return "#{words[0]} and #{words[1]}"
+  elsif words.length > 2
+    words[-1].insert(0 , "and ")
+  end
+  array.join(", ")    
 end
 
