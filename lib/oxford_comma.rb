f4b5c455bb3aeb172ae724185436d3c7c3a34f55
sasha walker <sashawalker31@gmail.com>
2020-01-29 14:25:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2
     return "#{words[0]} and #{words[1]}"
-  elsif words.length > 2
+  elsif array.length > 2
     words[-1].insert(0 , "and ")
   end
   array.join(", ")    
