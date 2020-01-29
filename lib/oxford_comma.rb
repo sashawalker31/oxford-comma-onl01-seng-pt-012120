e1246f942b9119a13bbd6cf892fffd46306ade63
sasha walker <sashawalker31@gmail.com>
2020-01-29 14:12:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,5 +9,5 @@ def oxford_comma(array=["kiwi", "durian"])
 end
 
 def oxford_comma(array=["kiwi", "durian", "starfruit"])
-  results=array.insert(2,"and").join(", ")
+  results=array.insert(-2,"and").join(", ")
 end
